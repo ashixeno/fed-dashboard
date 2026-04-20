51bfbeb0fce4eede7ba8f68fb3d1550782efd225
ashixeno <121361156+ashixeno@users.noreply.github.com>
2026-04-20 13:26:17 +0530

    Create page.tsx

---
+++ src/app/inflation/page.tsx
@@ -0,0 +1,13 @@
+export default function InflationPage() {
+  return (
+    <iframe
+      src="/inflation/index.html"
+      style={{
+        width: '100%',
+        height: '100vh',
+        border: 'none',
+        display: 'block',
+      }}
+    />
+  )
+}
